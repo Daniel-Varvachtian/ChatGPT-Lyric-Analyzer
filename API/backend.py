a8a2a32bf58d6c890b6c07163aebc5f588e0b27a
Daniel Varvachtian <varvacht@ualberta.ca>
2023-11-09 23:41:31 -0700

    updated backend

--- API/backend.py
+++ API/backend.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/api', methods = ['Get'])
+def return_ascii():
+    d = {}
+    inputchr = str(request.args['query'])
+    answer = str(ord(inputchr))
+    d['output'] = answer
+    return d
+
+if __name__ == "__main__":
+    app.run()
